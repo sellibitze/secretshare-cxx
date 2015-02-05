87d8b1957b2a45123d1bfa15538337a328423194
Sebastian Gesemann <s.gesemann@gmail.com>
2015-02-05 23:16:47 +0100

    add getrandom interface

---
+++ getrandom.hpp
@@ -0,0 +1,9 @@
+#ifndef INC_GUARD_K0IU7HK29CW2TVY9LVJDN961U6N0LW
+#define INC_GUARD_K0IU7HK29CW2TVY9LVJDN961U6N0LW
+
+#include "slice.hpp"
+
+void get_random(slice<unsigned char> fill);
+
+#endif /* include guard */
+
